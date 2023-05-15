2526daa443d620910e4e85492cfbb559ed903951
tomokimuratani <tomochi.2000.10.8@gmail.com>
2023-05-15 12:56:43 +0900

    フッター、ヘッダーの作成

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -38,95 +38,96 @@ export function AuthenticationForm(props: PaperProps) {
   });
 
   return (
-       <Affix position={{top:"25%",left:"25%",bottom:"25%",right:"25%"}}>
-
-    <Container size={420} >
-    <Paper radius="md" p="xl" withBorder {...props}>
-      <Text size="lg" weight={500}>
-      {type === "login"
+    <Affix position={{ top: "25%", left: "25%", bottom: "25%", right: "25%" }}>
+      <Container size={420}>
+        <Paper radius="md" p="xl" withBorder {...props}>
+          <Text size="lg" weight={500}>
+            {type === "login"
               ? "ようこそログインしてください"
               : "ようこそ登録してください"}
-      </Text>
+          </Text>
 
-      <Group grow mb="md" mt="md"> 
-        <GoogleButton radius="xl">Google</GoogleButton>
-        <TwitterButton radius="xl">Twitter</TwitterButton>
-              </Group>
+          <Group grow mb="md" mt="md">
+            <GoogleButton radius="xl">Google</GoogleButton>
+            <TwitterButton radius="xl">Twitter</TwitterButton>
+          </Group>
 
-      <Divider label="またはメールで続行してください" labelPosition="center" my="lg" />
+          <Divider
+            label="またはメールで続行してください"
+            labelPosition="center"
+            my="lg"
+          />
 
-      <form onSubmit={form.onSubmit(() => {})}>
-        <Stack>
-          {type === "register" && (
-            <TextInput
-            label="名前"
-            placeholder="あなたの名前"
-            value={form.values.name}
-            onChange={(event) =>
-              form.setFieldValue("name", event.currentTarget.value)
-            }
-            radius="md"
-            />
-            )}
+          <form onSubmit={form.onSubmit(() => {})}>
+            <Stack>
+              {type === "register" && (
+                <TextInput
+                  label="名前"
+                  placeholder="あなたの名前"
+                  value={form.values.name}
+                  onChange={(event) =>
+                    form.setFieldValue("name", event.currentTarget.value)
+                  }
+                  radius="md"
+                />
+              )}
 
-          <TextInput
-            required
-            label="メールアドレス"
-            
-            value={form.values.email}
-            onChange={(event) =>
-              form.setFieldValue("email", event.currentTarget.value)
-            }
-            error={form.errors.email && "Invalid email"}
-            radius="md"
-            />
+              <TextInput
+                required
+                label="メールアドレス"
+                value={form.values.email}
+                onChange={(event) =>
+                  form.setFieldValue("email", event.currentTarget.value)
+                }
+                error={form.errors.email && "Invalid email"}
+                radius="md"
+              />
 
-          <PasswordInput
-            required
-            label="パスワード"
-            placeholder="あなたのパスワードを入れてください"
-            value={form.values.password}
-            onChange={(event) =>
-              form.setFieldValue("password", event.currentTarget.value)
-            }
-            error={
-              form.errors.password &&
-              "Password should include at least 6 characters"
-            }
-            radius="md"
-            />
+              <PasswordInput
+                required
+                label="パスワード"
+                placeholder="あなたのパスワードを入れてください"
+                value={form.values.password}
+                onChange={(event) =>
+                  form.setFieldValue("password", event.currentTarget.value)
+                }
+                error={
+                  form.errors.password &&
+                  "Password should include at least 6 characters"
+                }
+                radius="md"
+              />
 
-          {type === "register" && (
-            <Checkbox
-            label="I accept terms and conditions"
-            checked={form.values.terms}
-            onChange={(event) =>
-              form.setFieldValue("terms", event.currentTarget.checked)
-            }
-            />
-            )}
-        </Stack>
+              {type === "register" && (
+                <Checkbox
+                  label="I accept terms and conditions"
+                  checked={form.values.terms}
+                  onChange={(event) =>
+                    form.setFieldValue("terms", event.currentTarget.checked)
+                  }
+                />
+              )}
+            </Stack>
 
-        <Group position="apart" mt="xl">
-          <Anchor
-            component="button"
-            type="button"
-            color="dimmed"
-            onClick={() => toggle()}
-            size="xs"
-            >
-            {type === "register"
-              ? "すでにアカウントお持ちの場合は、 ログイン"
-              : "アカウントをお持ちでない場合は、 登録"}
-          </Anchor>
-          <Button type="submit" radius="xl">
-            {upperFirst(type)}
-          </Button>
-        </Group>
-      </form>
-    </Paper>
-    </Container>
-               </Affix> 
-
+            <Group position="apart" mt="xl">
+              <Anchor
+                component="button"
+                type="button"
+                color="dimmed"
+                onClick={() => toggle()}
+                size="xs"
+              >
+                {type === "register"
+                  ? "すでにアカウントお持ちの場合は、 ログイン"
+                  : "アカウントをお持ちでない場合は、 登録"}
+              </Anchor>
+              <Button type="submit" radius="xl">
+                {upperFirst(type)}
+              </Button>
+            </Group>
+          </form>
+        </Paper>
+      </Container>
+    </Affix>
   );
 }
